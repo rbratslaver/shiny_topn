912a71f248dd4db0de37f9c70b28a818113ce905
Roman <romanb333@gmail.com>
2021-01-25 23:39:01 -0500

    updates

--- topn/app.R
+++ topn/app.R
@@ -29,14 +29,42 @@ pacman::p_load(tidyverse,shiny,shinydashboard,gt,skimr,janitor)
 
 
 
+# input vars -------------------------------------------------------------------
+
+mpg %>% select(!where(is.numeric)) %>% names()->vars
+c('mean','median')->agg
+mpg %>% select(where(is.numeric)) %>% names()->metrics
+
+
+
 # ui ----------------------------------------------------------------------
 
+header <- dashboardHeader(title = 'Top N MPG Dashboard')
 
-ui <- dashboardPage(
-    dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody()
-)
+
+sidebar <- dashboardSidebar(
+    selectInput('groupby_vars','select grouping variables',vars),
+    selectInput('agg_type','select aggregation',agg),
+    selectInput('metric','select metric',metrics),
+    sliderInput('topn_value',
+                "select desired topn",
+                min=0,
+                max=10,
+                value=1))
+
+
+
+
+body <- dashboardBody(
+  fluidRow(
+    box(tableOutput("topn_table"),width=4)
+  )
+)  
+
+
+ui <- dashboardPage(header,sidebar,body)
+
+  
 
 
 
@@ -44,9 +72,26 @@ ui <- dashboardPage(
 # server ------------------------------------------------------------------
 
 
-server <- function(input, output) {}
-
-
+server <- function(input, output) {
+  output$topn_table <- renderTable(
+    mpg %>%
+        group_by(manufacturer,model) %>%
+        summarise(mean_cty = mean(cty),.groups='drop_last') %>%
+        mutate(model = case_when(dense_rank(desc(mean_cty))<=1~model,TRUE~'others')) %>%
+        mutate(index = case_when(
+            model=='others'~1,
+            TRUE~0
+        )) %>%
+        ungroup() %>%
+        group_by(manufacturer,model,index) %>%
+        summarise(mean_cty = mean(mean_cty)) %>%
+        arrange(manufacturer,index,desc(mean_cty)) %>%
+        select(-index) %>%
+        ungroup() %>%
+        gt() %>%
+        fmt_number('mean_cty',decimals = 1)
+  )
+}
 
 
 # run app -----------------------------------------------------------------
